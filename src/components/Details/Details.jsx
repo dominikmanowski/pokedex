3bbdd8fb23595f3c9dd35e4b4b9c031e9812b2ca
Dominik Manowski <dominik.manowski@jit.team>
2019-11-28 16:29:43 +0100

    Add go back link

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import Loader from '../Loader/Loader';
-import { withRouter } from 'react-router-dom';
-import './Details.css'
+import { Link, withRouter } from 'react-router-dom';
+import './Details.css';
 
 const Details = ({ pokemons, match }) => {
   const pokemon = useMemo(
@@ -11,15 +11,16 @@ const Details = ({ pokemons, match }) => {
 
   return (
     <>
+      <Link to='/'><button class="go-back">&#x21A9;</button></Link>
       {!pokemon && <Loader />}
       {pokemon && (
-        <div className='detail-box'>
+        <div className="detail-box">
           <h2 className="name">{pokemon.name}</h2>
           <img src={pokemon.imageUrl} alt={pokemon.name} />
         </div>
       )}
     </>
   );
-}
+};
 
 export default withRouter(Details);
