565811b6492969d676a0deff61a5d79dfc7414ce
Dominik Manowski <manowski@protonmail.com>
2019-11-28 20:04:22 +0100

    Add ErrorBoundaries

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, memo } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ErrorBoundaries from './components/ErrorBoundaries/ErrorBoundaries';
 import Header from './components/Header/Header';
 import Deck from './components/Deck/Deck';
 import Details from './components/Details/Details';
@@ -9,38 +10,47 @@ import { URL } from './constants';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
-  const [favorites, setFavorites] = useState([])
-  console.log("TCL: App -> favorites", favorites)
+  const [favorites, setFavorites] = useState([]);
+  console.log('TCL: App -> favorites', favorites);
 
-  const handleAddToFavorite = (id) => {
+  const handleAddToFavorite = id => {
     if (favorites.includes(id)) {
-      setFavorites(pokemons.find(pokemon => pokemon.id === id))
+      setFavorites(pokemons.find(pokemon => pokemon.id === id));
     }
-    
-  }
+  };
 
-  const query = 'cards';
+  const query = 'cards?supertype=pokemon/';
 
   useEffect(() => {
-    axios.get(`${URL}${query}`).then(res => {
-      const cards = res.data.cards.filter(card => card.supertype === "Pokémon")
-      setPokemons(cards);
-    });
+    axios
+      .get(`${URL}${query}`)
+      .then(res => {
+        setPokemons(res.data.cards);
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
   }, []);
 
   return (
     <>
-      <Header />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Deck pokemons={pokemons} />
-          </Route>
-          <Route path="/pokemon/:id">
-            <Details pokemons={pokemons} handleAddToFavorite={handleAddToFavorite} favorites={favorites}/>
-          </Route>
-        </Switch>
-      </Router>
+      <ErrorBoundaries>
+        <Header />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Deck pokemons={pokemons} />
+            </Route>
+            <Route path="/pokemon/:id">
+              <Details
+                pokemons={pokemons}
+                handleAddToFavorite={handleAddToFavorite}
+                favorites={favorites}
+              />
+            </Route>
+          </Switch>
+        </Router>
+      </ErrorBoundaries>
     </>
   );
 }
